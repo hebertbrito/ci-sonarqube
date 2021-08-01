c3c22d5f685138984441b808c2381a171301cbdf
Hebert Brito <hebertfelipe.97@outlook.com.br>
2021-08-01 18:21:14 -0300

    ci: :construction_worker: add sonar.properties

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 function sum(a, c) {
   return a + c;
 }
-
-// function minus(a, c) {
-//   return a - c;
-// }
-
-// function times(a, c) {
-//   return a * c;
-// }
-
-// function division(a, d) {
-//   return a / d;
-// }
 module.exports = {
   sum: sum
 };
