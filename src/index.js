6e300e4572134009ab5bbf183285681ad0623084
Hebert Brito <hebertfelipe.97@outlook.com.br>
2021-08-01 18:06:49 -0300

    ci: :construction_worker: add sonar cloud

--- src/index.js
+++ src/index.js
@@ -2,20 +2,17 @@ function sum(a, c) {
   return a + c;
 }
 
-function minus(a, c) {
-  return a - c;
-}
+// function minus(a, c) {
+//   return a - c;
+// }
 
-function times(a, c) {
-  return a * c;
-}
+// function times(a, c) {
+//   return a * c;
+// }
 
-function division(a, d) {
-  return a / d;
-}
+// function division(a, d) {
+//   return a / d;
+// }
 module.exports = {
-  sum: sum,
-  minus: minus,
-  times:times,
-  division:division
+  sum: sum
 };
--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,4 @@
-const {sum, minus, times, division} = require('./index');
+const {sum} = require('./index');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
